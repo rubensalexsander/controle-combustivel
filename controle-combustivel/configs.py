315b858c1932c23b90116d3a204b7cbe8bf038c6
Alexsander Rubens <rubensalexsander0001@gmail.com>
2025-04-20 15:35:24 -0300

    Commit

--- controle-combustivel/configs.py
+++ controle-combustivel/configs.py
@@ -1,15 +1,11 @@
 # App sets ------------------------------------------------------
 install_path = r'C:/Users/Pichau/Documents/dev/controle-combustivel/controle-combustivel'
-cia_name = 'Delegacia A - Cidade exemplo'
-resolution_main = [410, 285]
-resolution_configs = [600, 400]
-
-
-# Google sheet sets ---------------------------------------------
 spread_sheet_id = '19ogjTzqBT0_v-N_7EGmL2eyP22-_E-LhOCB6SR206OA'
+
+# Configs que serão excluidas nas próximas atualizações
+cia_name = 'Delegacia A - Cidade exemplo'
 sheet_pag_name = 'plan1'
 sheet_range = 'B:G'
-
 sheet_map = { #converted to python list (-1)
         'prefixo':0,
         'datahora':1,
@@ -19,6 +15,9 @@ sheet_map = { #converted to python list (-1)
         'origem':5
     }
 
-# Standard plan sets -----------------------------------------------
+# No change configs -----------------------------------------------
 standard_plan_pag = 'plan1'
 standard_plan_range = 'A:K'
+
+resolution_main = [410, 285]
+resolution_configs = [600, 400]
